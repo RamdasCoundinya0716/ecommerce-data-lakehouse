cdc07d531dd750b5f1d64b9ce7c011eb5496d7fa
Ramdas <rcoundinya@gmail.com>
2024-12-28 11:52:30 +0530

    added data generation script and terraform scripts to create resource group. Next steps to be done!

---
+++ terraform/resource-grp-creation/main.tf
@@ -0,0 +1,20 @@
+# Provider Configuration
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+# Resource Group
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Outputs
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.main.location
+}
---
+++ terraform/resource-grp-creation/variables.tf
@@ -0,0 +1,13 @@
+variable "location" {
+  default = "your-location"
+  description = "The Azure region where the resources will be created."
+}
+
+variable "resource_group_name" {
+  default = "your-resource-group-name"
+  description = "The name of the resource group."
+}
+variable "subscription_id" {
+  default = "your-subscription-id"
+  description = "The Azure subscription ID."
+}
